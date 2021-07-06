4d76577c9d3c9859678469aaf06a2385790314b2
Pamella Santos <pamellaolys@gmail.com>
2021-07-06 19:30:12 -0300

    Atualização do blogPessoal, com security implementado

--- BlogPessoal/src/main/java/br/com/generation/blogpessoal/model/Postagem.java
+++ BlogPessoal/src/main/java/br/com/generation/blogpessoal/model/Postagem.java
@@ -38,6 +38,10 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Usuario usuario;
+	
 	
 	public long getId() {
 		return id;
